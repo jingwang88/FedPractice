72803242e631904f1f3c37195de9d00956994b2a
jingwang88 <wj154419@163.com>
2016-01-20 23:16:41 +0800

    update util.js

--- bdLife/util3.js
+++ bdLife/util3.js
@@ -64,3 +64,71 @@ console.log(outerPose.y);
 console.log(innerPose.x);
 console.log(innerPose.y);
 
+/*--------------- 实现简单$ -------------*/
+// 实现一个简单的Query
+
+function $(selector) {
+    var selectors = selector.split(' ');
+    if(selectors.length<2) {
+        var regId = /^#(\w+)$/g, regClass = /^.(\w+)$/g, regTag = /^(\w+)$/g,
+            regAttr = /^\[(([0-9a-zA-z]|-)+)\]$/g, regAttrVal = /^[(.+)=(.+)]$/g;
+        if(regId.test(selector)) {
+            return document.getElementById($1);
+        }else if(regClass.test(selector)) {
+            return document.getElementsByClassName($1)[0];
+        }else if(regTag.test(selector)) {
+            return document.getElementsByTagName($1)[0];
+        }else if(regAttr.test(selector)) {
+            var nodeList = document.getElementsByTagName('*');
+            var nodeArr = [];
+            for(var i=0, ele; ele=nodeList[i]; i++) {
+                if(ele.getAttribute($1)) {
+                    nodeArr.push(ele);
+                }
+            }
+            return nodeArr[0];
+        }else if(regAttrVal.test(selector)) {
+            var nodeList = document.getElementsByTagName('*');
+            var nodeArr = [];
+            for(var i=0, ele; ele=nodeList[i]; i++) {
+                if(ele.getAttribute($1) == $2) {
+                    nodeArr.push(ele);
+                }
+            }
+            return nodeArr[0];
+        }
+    }else {
+        function(selectors) {
+            for(var i=selectors.length; i>1; i--) {
+                if($(selectors[i-1]).parentNode == $(selectors[i-2]) && i == 2) {
+                    return $(selectors[selectors.length-1]);
+                }
+            }
+        }
+    }
+}
+//使用微博首页测试
+console.log($("#plc_main"));
+console.log($(".W_f14"));
+console.log($("p"));
+console.log($("[fixed-box]"));
+console.log($("[fixed-box=true]"));
+
+
+// 可以通过id获取DOM对象，通过#标示，例如
+$("#adom"); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
+
